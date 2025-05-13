97a2450c4703cc688ae2abf5750ce3aabd503194
Mazaneazy <yvan_many@yahoo.fr>
2025-05-13 11:32:39 +0100

    Create AddDossier.tsx

---
+++ src/components/dossiers/AddDossier.tsx
@@ -0,0 +1,13 @@
+// src/components/dossiers/AddDossier.tsx
+import React from 'react';
+
+const AddDossier: React.FC = () => {
+  return (
+    <div>
+      <h2>Ajouter un Dossier</h2>
+      {/* Formulaire ou contenu ici */}
+    </div>
+  );
+};
+
+export default AddDossier;
