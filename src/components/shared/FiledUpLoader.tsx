f88325c22cee84962ab0df704c3262a3db9734cb
Mazaneazy <yvan_many@yahoo.fr>
2025-05-13 11:50:18 +0100

    Create FiledUpLoader.tsx

---
+++ src/components/shared/FiledUpLoader.tsx
@@ -0,0 +1,12 @@
+// src/components/shared/FileUploader.tsx
+import React from 'react';
+
+const FileUploader: React.FC = () => {
+  return (
+    <div>
+      <input type="file" />
+    </div>
+  );
+};
+
+export default FileUploader;
