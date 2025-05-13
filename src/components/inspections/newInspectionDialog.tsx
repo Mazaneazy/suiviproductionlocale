122481639b04f3942c70f5f8d1caca07f78c036f
Mazaneazy <yvan_many@yahoo.fr>
2025-05-13 13:16:14 +0100

    Create newInspectionDialog.tsx

---
+++ src/components/inspections/newInspectionDialog.tsx
@@ -0,0 +1,12 @@
+// src/components/inspections/NewInspectionDialog.tsx
+import React from 'react';
+
+const NewInspectionDialog = () => {
+  return (
+    <div>
+      <p>Composant NewInspectionDialog à implémenter.</p>
+    </div>
+  );
+};
+
+export default NewInspectionDialog;
